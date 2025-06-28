868c4680d2ac26e5cfe2398a07ebb5ae8e2aef84
mohammedhassa15 <mohammedhassan0041@gmail.com>
2025-06-28 22:50:22 +0300

    task completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code here
+// Define Breakfast class
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+// Define Lunch class
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+// Define Dinner class
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = dessert;
+  }
+}
